e6bc99317ecc9e45eb547d60704d99ee7e94eb4c
liuyaanng <1757465118@qq.com>
2024-05-13 11:09:48 +0800

    some changes

--- zsh/proxy.zsh
+++ zsh/proxy.zsh
@@ -1,2 +1,2 @@
-# export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
-export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087;export ALL_PROXY=socks5://127.0.0.1:1080
+export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
+# export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087;export ALL_PROXY=socks5://127.0.0.1:1080
