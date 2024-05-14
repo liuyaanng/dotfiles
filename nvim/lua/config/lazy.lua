c25aac5742b0f9d636049186270917604b225066
liuyaanng <1757465118@qq.com>
2024-05-14 10:22:26 +0800

    add some vim plug and config yabai restart with borders

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -90,6 +90,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.coding.copilot" },
