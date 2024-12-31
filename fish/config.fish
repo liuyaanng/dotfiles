85e57ad151e7d424ba1c39c15d3b1f443fc1ef0d
liuyaanng <1757465118@qq.com>
2024-12-31 11:15:58 +0800

    add fish shell

---
+++ fish/config.fish
@@ -0,0 +1,16 @@
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# alias 
+alias c clear
+alias gc "git clone"
+alias vim nvim
+alias ff fastfetch
+alias lg lazygit
+alias show "defaults write com.apple.finder CreateDesktop true && killall Finder"
+alias hide "defaults write com.apple.finder CreateDesktop false && killall Finder"
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "ls -l"
+alias lla "ll -A"
