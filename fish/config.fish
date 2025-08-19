95d8d8febfb521373f4e656889495d99df5c927b
liuyaanng <1757465118@qq.com>
2025-08-19 10:06:26 +0800

    feat(fish): use nvm

--- fish/config.fish
+++ fish/config.fish
@@ -14,8 +14,23 @@ alias ls "ls -p -G"
 alias la "ls -A"
 alias ll "ls -l"
 alias lla "ll -A"
+alias cc "ccusage blocks --live"
+alias ide='tmux split-window -h -d -l 25%; tmux split-window -v -l 20%; tmux split-window -h -l 50%'
 
 
 # proxy 
 set -x https_proxy http://127.0.0.1:7890
 set -x http_proxy http://127.0.0.1:7890
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/anaconda3/bin/conda
+    eval /opt/anaconda3/bin/conda "shell.fish" hook $argv | source
+else
+    if test -f "/opt/anaconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/anaconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH /opt/anaconda3/bin $PATH
+    end
+end
+# <<< conda initialize <<<
